be6cc2dba518f6758c0bb154765e9d534e3cc6a4
TEH YI HEN <tehyihen1994@gmail.com>
2025-12-05 12:48:11 +0800

    rate limiting updated

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -8,8 +8,29 @@ export const protectRoute = async (req, res, next) => {
         const token = req.cookie?.jwt;
         if(!token) return res.status(401).json({message:"Unauthorized - No token provided"});
 
-        const decoded = jwt.verify(token, ENV.JWT_SECRET);
-        if(!decoded) return res.status(401).json({message:"Unauthorized - Invalid token"});
+        //const decoded = jwt.verify(token, ENV.JWT_SECRET);
+        //if(!decoded) return res.status(401).json({message:"Unauthorized - Invalid token"});
+
+        //After codeRabbit
+        const { JWT_SECRET } = ENV;
+
+        if (!JWT_SECRET) {
+         console.error("JWT_SECRET is not configured");
+         return res.status(500).json({ message: "Server misconfiguration" });
+        }
+
+        let decoded;
+        try {
+            decoded = jwt.verify(token, JWT_SECRET);
+        } catch (err) {
+        const msg = err.name === "TokenExpiredError"
+        ? "Unauthorized - Token expired"
+        : "Unauthorized - Invalid token";
+
+        return res.status(401).json({ message: msg });
+        };
+
+
 
         const user = await User.findById(decoded.userId).select("-password");
         if(!user) return res.status(404).json({message:"User not found"});
