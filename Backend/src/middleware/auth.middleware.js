8c05d4faaf2a8cf39caa1bf1a8a1762421010e37
TEH YI HEN <tehyihen1994@gmail.com>
2026-01-18 14:24:39 +0800

    hide console error

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -6,8 +6,12 @@ export const protectRoute = async (req, res, next) => {
 
     try {
         const token = req.cookies?.jwt;
-        if(!token) return res.status(401).json({message:"Unauthorized - No token provided"});
-
+        //if(!token) return res.status(401).json({message:"Unauthorized - No token provided"});
+        if(!token) {
+            
+            req.user = null;
+            return next();
+        }
         //const decoded = jwt.verify(token, ENV.JWT_SECRET);
         //if(!decoded) return res.status(401).json({message:"Unauthorized - Invalid token"});
 
